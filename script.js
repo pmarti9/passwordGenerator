96fb4381f2808012f6b15ab945fc6063ddaa0c80
pmarti99 <parkerjmartin883@gmail.com>
2020-03-21 22:15:15 -0600

    variables for different strings

--- script.js
+++ script.js
@@ -10,5 +10,25 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+let hasSpecialCharacters = confirm("Special Charatiers");
+const specialCharacters = ["!","@","#","$","%","^","&","*","(","0"];
+let hasNumbers = confirm("Has Numbers");
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
+let hasLetters = confirm("Has letters");
+const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+let hasUCLetters = confirm("Has letters");
+const ucLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const randomLetters = letters[Math.floor(Math.random()*letters.length)];
+let pwlength = prompt("How long do you want your password to be");
+
+
+console.log(pwLength);
+//GENERATE PASSWORD
+function generatePassword (specialCharacters, numbers, letters, ucLetters, pwlength) {
+
+};
