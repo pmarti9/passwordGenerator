8d3f9c3bbce6cae281886a5c56aad68371b9cc9a
pmarti99 <parkerjmartin883@gmail.com>
2020-04-22 20:09:25 -0600

    completed password generator

--- script.js
+++ script.js
@@ -3,36 +3,49 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var characters = prompt("how many characters do you want your password to be? (between 8-128)");
+  if(characters < 8 || characters > 128){
+    return passwordText.value = "Pick a number between 8-128";
+  };
+  var lowercase = [..."abcdefghijklmnopqrstuvwxyz"];
+  var uppercase = [..."abcdefghijklmnopqrstuvwxyz".toUpperCase()];
+  var numeric = [..."1234567890"];
+  var specChar = [..."!@#$%^&*()"];
 
-  passwordText.value = password;
+  var hasLowerCase = confirm("Has lowercase letters");
+  var hasUpperCase = confirm("Has uppercase letters");
+  var hasNumbers = confirm("Has numbers");
+  var hasSpecChar = confirm("Has special characters");
+  if(!hasLowerCase && !hasUpperCase && !hasNumbers && !hasSpecChar) {
+    return passwordText.value = "Please select password criteria";
+  };
+
+  var selectedCriteria = [];
+
+  if(hasLowerCase){
+    selectedCriteria.push(...lowercase);
+  };
+  if(hasUpperCase){
+    selectedCriteria.push(...uppercase);
+  };
+  if(hasNumbers){
+    selectedCriteria.push(...numeric);
+  };
+  if(hasSpecChar){
+    selectedCriteria.push(...specChar);
+  };
+  
+  let password = "";
+ 
+  for(let i = 0; i < parseInt(characters); i++) {
+    password += selectedCriteria[Math.floor(Math.random() * selectedCriteria.length)];
+  }
+
+  return passwordText.value = password;
 
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-let hasSpecialCharacters = confirm("Special Charatiers");
-const specialCharacters = ["!","@","#","$","%","^","&","*","(","0"];
-let hasNumbers = confirm("Has Numbers");
-const numbers = ["0","1","2","3","4","5","6","7","8","9"];
-let hasLetters = confirm("Has letters");
-const letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-let hasUCLetters = confirm("Has letters");
-const ucLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const randomLetters = letters[Math.floor(Math.random()*letters.length)];
-let pwlength = prompt("How long do you want your password to be");
-
-
-
-
-//GENERATE PASSWORD
-function generatePassword (specialCharacters, numbers, letters, ucLetters, pwlength) {
-if(hasLetters)
-    confirm(hasUCLetters);
-};
-
-generatePassword();
